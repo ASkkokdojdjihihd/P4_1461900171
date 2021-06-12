566101cec6f715490ccf04bc5234ab2ff49a7745
ASkkokdojdjijijd <amararekardukis@gmail.com>
2021-06-12 13:55:00 +0700

    pertemuan 4

---
+++ database/factories/JenisBukuFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\JenisBuku;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JenisBukuFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = JenisBuku::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/RakBukuFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\RakBuku;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RakBukuFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = RakBuku::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
